fb853f94cf1a7123748e8fa6abb7ff844ffb8c19
Adam Salhi <adam06genius@gmail.com>
2021-06-15 18:16:46 +0100

    Added portals

--- src/Objects/Portal2D.gd
+++ src/Objects/Portal2D.gd
@@ -12,6 +12,8 @@ func _get_configuration_warning() -> String:
 	return "The next scene property can't be empty" if not next_scene else ""
 
 func teleport() -> void:
+	anim_player.play("close")
+	yield(anim_player, "animation_finished")
 	anim_player.play("fade_in")
 	yield(anim_player, "animation_finished")
 	get_tree().change_scene_to(next_scene)
